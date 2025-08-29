f72d47cbd98945d9775ca855a673a9b16962e400
mingging27 <mingging227@gmail.com>
2025-08-29 23:02:38 +0900

    2일차, 5장 연습문제

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -33,5 +37,25 @@ public class MemberController {
         log.info(saved.toString());
 
         return "members/new";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id="+id);
+
+        Member member = memberRepository.findById(id).orElse(null);
+
+        model.addAttribute("member", member);
+
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        List memberList = memberRepository.findAll();
+
+        model.addAttribute("memberList", memberList);
+
+        return "members/index";
     }
 }
--- src/main/java/com/example/firstproject/repository/MemberRepository.java
+++ src/main/java/com/example/firstproject/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 // 생성한 엔티티 객체를 DB에 넣는 등 DB와 상호작용
 public interface MemberRepository extends CrudRepository<Member, Long> {
+    @Override
+    ArrayList<Member> findAll();
 }
