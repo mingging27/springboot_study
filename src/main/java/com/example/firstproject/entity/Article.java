322ca110f554a859adde0a471474c3742d929825
mingging27 <mingging227@gmail.com>
2025-08-30 03:10:49 +0900

    3일차, 9장 개념 및 연습문제

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +13,7 @@ import lombok.ToString;
 @ToString
 public class Article {
     @Id //대푯값 지정
-    @GeneratedValue //대푯값인 id를 자동으로 생성
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //id 자동 생성 전략
     private Long id;
     @Column //각각 선언
     private String title;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT into article(id, title, content) values(1, '가가가가', '1111');
-INSERT into article(id, title, content) values(2, '나나나나', '2222');
-INSERT into article(id, title, content) values(3, '다다다다', '3333');
-INSERT into member(id, email, password) values(1, 'mj1@gmail.com', '1111');
-INSERT into member(id, email, password) values(2, 'mj2@gmail.com', '2222');
-INSERT into member(id, email, password) values(3, 'mj3@gmail.com', '3333');
+INSERT into article(title, content) values('가가가가', '1111');
+INSERT into article(title, content) values('나나나나', '2222');
+INSERT into article(title, content) values('다다다다', '3333');
+INSERT into member(email, password) values('mj1@gmail.com', '1111');
+INSERT into member(email, password) values('mj2@gmail.com', '2222');
+INSERT into member(email, password) values('mj3@gmail.com', '3333');
 
